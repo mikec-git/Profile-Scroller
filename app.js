32710618741bb2930f01b35cded9bcc4df6e3323
Mike Choi <mike.choi@live.ca>
2018-09-20 13:43:05 -0700

    Initial Commit - Has basic bootstrap markup

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        gender: 'Male',
+        lookingfor: 'Female',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {
+        name: 'Jen Smith',
+        age: 26,
+        gender: 'Female',
+        lookingfor: 'Male',
+        location: 'Miami Fl',
+        image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    }
+    {
+        name: 'William Johnson',
+        age: 38,
+        gender: 'Male',
+        lookingfor: 'Female',
+        location: 'Lynn MA',
+        image: 'https://randomuser.me/api/portraits/men/83.jpg'
+    }
+];
